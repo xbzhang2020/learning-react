bc57132b08c6901cfbbaaf4e74833a0f18ad74d2
zhangxingbin <zhangxingbin@meituan.com>
2022-01-14 09:36:44 +0800

    feat: 代码优化

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 // import MyContext from './components/context'
 // import MyState from './components/state'
 // import MyRedux from './components/redux'
-import MyMobx from "./components/mobx";
+// import MyMobx from "./components/mobx";
 
 function App() {
   return (
-    <div className="App">
-      <MyMobx />
+    <div className='App'>
+      <h1>Hello React</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

f811597067925820b225920028553108968515fd
iceatlas <xbzhang2020@icloud.com>
2022-01-15 11:47:38 +0800

    feat: hooks

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 // import MyState from './components/state'
 // import MyRedux from './components/redux'
 // import MyMobx from "./components/mobx";
+import HookDemo from './hooks'
 
 function App() {
   return (
     <div className='App'>
       <h1>Hello React</h1>
+      <HookDemo />
     </div>
   )
 }
---
+++ src/hooks/index.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div>xxx</div>
+}
+
+export default App

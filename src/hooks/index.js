7554e5c3bfd38efd10b9920910861f57cdc61dc8
iceatlas <xbzhang2020@icloud.com>
2022-01-22 19:25:02 +0800

    feat: 自定义钩子

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,36 +1,76 @@
 import { useState } from 'react'
 import { StarFilled } from '@ant-design/icons'
 
+export function useInput(initValue) {
+  const [value, setValue] = useState(initValue)
+  return [
+    {
+      value,
+      onChange: event => setValue(event.target.value)
+    },
+    () => setValue(initValue)
+  ]
+}
+
 function App() {
+  const [usernameProps, resetUsername] = useInput('')
+  const [rate, setRate] = useState(0)
+  const [commentProps, resetComment] = useInput('')
+
+  function submit(event) {
+    event.preventDefault()
+
+    // 模拟提交表单行为
+    console.log({ username: usernameProps.value, rate, comment: commentProps.value })
+  }
+
+  function reset(event) {
+    event.preventDefault()
+    resetUsername()
+    setRate(0)
+    resetComment()
+  }
+
   return (
-    <div>
-      <StarRating totalStars={5} />
-    </div>
+    <form>
+      <div>
+        <label>用户名称：</label>
+        <br />
+        <input {...usernameProps} type="text" />
+      </div>
+      <div>
+        <label>评分：</label>
+        <StarRating value={rate} onChange={setRate} />
+      </div>
+      <div>
+        <label>评论：</label>
+        <br />
+        <textarea {...commentProps} />
+      </div>
+      <button onClick={submit}>提交</button>
+      <button onClick={reset}>重置</button>
+    </form>
   )
 }
 
-export function StarRating({ totalStars = 5 }) {
-  const [selectedStars, setSelectedStars] = useState(0)
+export function StarRating({ value = 0, total = 5, onChange = f => f }) {
   return (
     <div>
-      {new Array(totalStars).fill('').map((item, index) => (
+      {new Array(total).fill('').map((item, index) => (
         <Star
           key={index}
-          selected={index < selectedStars}
-          onSelect={() => {
-            setSelectedStars(index + 1)
+          selected={index < value}
+          onChange={() => {
+            onChange(index + 1)
           }}
         />
       ))}
-      <p>
-        {selectedStars} of {totalStars} stars
-      </p>
     </div>
   )
 }
 
-export function Star({ selected = false, onSelect = (f) => f }) {
-  return <StarFilled style={{ color: selected ? 'red' : 'gray' }} onClick={onSelect} />
+export function Star({ selected = false, onChange = f => f }) {
+  return <StarFilled style={{ color: selected ? 'red' : 'gray' }} onClick={onChange} />
 }
 
 export default App

480127b5b92014df0345de394517ca32ef8b2b52
iceatlas <xbzhang2020@icloud.com>
2022-01-22 17:09:27 +0800

    feat: 评分组件

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@
 // import MyState from './components/state'
 // import MyRedux from './components/redux'
 // import MyMobx from "./components/mobx";
-// import HookDemo from './hooks'
-import RouterDemo from './router'
+import HooksDemo from './hooks'
+// import RouterDemo from './router'
 
 function App() {
   return (
     <div className="App">
       {/* <h1>Hello React, learn react</h1> */}
-      <RouterDemo />
+      <HooksDemo />
+      {/* <RouterDemo /> */}
     </div>
   )
 }
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,36 @@
+import { useState } from 'react'
+import { StarFilled } from '@ant-design/icons'
+
 function App() {
-  return <div>xxx</div>
+  return (
+    <div>
+      <StarRating totalStars={5} />
+    </div>
+  )
+}
+
+export function StarRating({ totalStars = 5 }) {
+  const [selectedStars, setSelectedStars] = useState(0)
+  return (
+    <div>
+      {new Array(totalStars).fill('').map((item, index) => (
+        <Star
+          key={index}
+          selected={index < selectedStars}
+          onSelect={() => {
+            setSelectedStars(index + 1)
+          }}
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
+    </div>
+  )
+}
+
+export function Star({ selected = false, onSelect = (f) => f }) {
+  return <StarFilled style={{ color: selected ? 'red' : 'gray' }} onClick={onSelect} />
 }
 
 export default App

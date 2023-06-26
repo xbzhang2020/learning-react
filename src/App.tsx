dd01aa9f5fcd321188980a5e116b70016d924e36
xbzhang2020 <xbzhang2020@icloud.com>
2023-06-26 21:51:15 +0800

    feat: useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 // import MyContext from './components/context'
 // import MyState from './components/state'
 // import MyRedux from './components/redux'
-
-import HooksDemo from './hooks'
+// import HooksDemo from './hooks'
 // import RouterDemo from './router'
+import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
       {/* <h1>Hello React, learn react</h1> */}
-      <HooksDemo />
+      {/* <HooksDemo /> */}
       {/* <RouterDemo /> */}
+      <button onClick={() => setCount(count + 1)}>{count}</button>
     </div>
   )
 }
